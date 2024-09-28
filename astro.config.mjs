ce8a381cbb2ccefad56114dc8190c775be95583e
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-09-28 19:56:44 +0600

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,16 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.steameracademy.me/',
+
   integrations: [react(), tailwind({
     applyBaseStyles: false, 
-  })]
+  })],
+
+  output: 'server',
+  adapter: netlify()
 });
