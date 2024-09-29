71ca391606d794db4f112f183a9d7d98570a7c8c
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-09-29 18:30:13 +0600

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
-
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.steameracademy.me/',
+  site: "https://www.steameracademy.me/",
 
-  integrations: [react(), tailwind({
-    applyBaseStyles: false, 
-  })],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 
-  output: 'server',
-  adapter: netlify()
+  output: "server",
+  adapter: netlify(),
 });
