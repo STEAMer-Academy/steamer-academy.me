c5c8a75f7103cbeb74e9d51adbe22c0654982c8c
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-02 17:59:19 +0600

    PurgeCSS Removed, Causing No css

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-import withPurgeCss from 'next-purgecss';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withPurgeCss(nextConfig);
+export default nextConfig;
 
