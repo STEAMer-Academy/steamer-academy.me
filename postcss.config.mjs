38e82d46f66d0127858522bb3a320ffe785c19a3
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-02 17:40:43 +0600

    PurgeCSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,35 +1,8 @@
-import withCSS from '@zeit/next-css';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack(config, { dev, isServer }) {
-    if (!dev && !isServer) {
-      // Optimize and strip unused CSS in production
-      config.module.rules.push({
-        test: /\.(css)$/,
-        use: [
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: {
-                  'postcss-flexbugs-fixes': {},
-                  'postcss-preset-env': {
-                    autoprefixer: {
-                      flexbox: 'no-2009',
-                    },
-                  },
-                },
-              },
-            },
-          },
-        ],
-      });
-    }
-    return config;
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
   },
 };
 
-export default withCSS(nextConfig);
-
+export default config;
