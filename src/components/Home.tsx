a47255a3e4a98d55a27032c29d55d3b7cb163951
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-03 23:11:15 +0600

    Lazy....

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -34,6 +34,7 @@ export default function Home() {
             width={1920}
             height={1080}
             sizes="100vw"
+            loading="eager"
           />
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <motion.div
