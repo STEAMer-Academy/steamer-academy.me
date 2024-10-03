11432e9ba52f9834a8cff4a9c0e0370f8902d9e1
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-03 20:25:13 +0600

    Properly Size Image in <Image/> Component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -25,14 +25,16 @@ export default function Home() {
       </Head>
 
       <div className="space-y-16">
-        <section className="relative h-[600px]">
+        <section className="relative h-[600px] overflow-hidden">
           <Image
             src="/placeholder.webp"
             alt="A world of possibilities"
-            objectFit="cover"
+            objectFit= "cover"
             loading="eager"
             priority={true}
-            sizes="fill"
+            width={1920}
+            height={1080}
+            sizes="100vw"
           />
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <motion.div
