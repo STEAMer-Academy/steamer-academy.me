3287390380a7622f83c418309691f82914c0f43a
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-03 19:59:42 +0600

    Properly Size Image in <Image/> Component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -32,7 +32,7 @@ export default function Home() {
             objectFit="cover"
             loading="eager"
             priority={true}
-            layout="fill"
+            sizes="fill"
           />
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <motion.div
