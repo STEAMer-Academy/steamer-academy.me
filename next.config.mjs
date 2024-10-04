57f87d726eda7e39a4e1c0b3e3c9a9a6e8dfdb30
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-04 18:23:41 +0600

    Fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+
+	async redirects() {
+		return [
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "steamer-academyme.netlify.app",
+					},
+				],
+				destination: "https://www.steameracademy.me/:path*",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
