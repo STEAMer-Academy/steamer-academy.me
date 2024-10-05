9a0c488c2ad2fe555d178a1adfabf5270f20e4f4
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-05 15:34:36 +0600

    Fixed 404 Problem

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
 import Page404 from "../components/NotFound";
+import { useState, useEffect } from "react";
 
 export default function Custom404() {
+
+  const [isClient, setIsClient] = useState(false)
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null
+  }
+
   return (
     <>
       <Head>
