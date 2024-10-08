756731641d178b4b44455af9037cdf3af1ea2c8b
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-08 20:17:33 +0600

    Checking

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,19 @@ const config = {
       },
     },
     extend: {
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from:{
+             backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
