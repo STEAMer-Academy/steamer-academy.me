a904c560624de97a4f130a836680007a998f0474
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-08 20:14:09 +0600

    Checking

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -104,7 +104,7 @@ export default function Home() {
                 STEAMer Academy is here to guide you on your learning journey
               </p>
               <Link href="https://discord.gg/Kqpbawj9KU" passHref>
-                <Button size="lg">Get started</Button>
+                <Button size="lg" className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">Get started</Button>
               </Link>
             </motion.div>
           </div>
