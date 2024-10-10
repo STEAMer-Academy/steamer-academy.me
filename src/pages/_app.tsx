319eef31baeea92770d2c6e2f625a9575522d1e4
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-10 18:56:04 +0600

    Partytown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,23 +7,22 @@ import Head from "next/head";
 import { Partytown } from "@builder.io/partytown/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const title = pageProps.title || "My Next.js App"; // Ensure consistent title
-  return (
-    <>
-      <Head>
-        <title>{title}</title>
-        {/* Include any additional meta tags or links here */}
-        <Partytown debug={true} forward={["dataLayer.push"]} />
-        <meta name="viewport" content="width=device-width" />
-      </Head>
-      <ThemeScript />
-      <Component {...pageProps} />
-      {/* Google Analytics */}
-      <script type="text/partytown">
-        <GoogleAnalytics gaId="G-DD3FHBDSP0" />
-      </script>
-    </>
-  );
+	const title = pageProps.title || "My Next.js App"; // Ensure consistent title
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+				{/* Include any additional meta tags or links here */}
+				<Partytown debug={true} forward={["dataLayer.push"]} />
+				<script type="text/partytown">
+					<GoogleAnalytics gaId="G-DD3FHBDSP0" />
+				</script>
+				<meta name="viewport" content="width=device-width" />
+			</Head>
+			<ThemeScript />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
