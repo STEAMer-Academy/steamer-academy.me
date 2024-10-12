f4819001c22c80faaefe2f62040fd2fc889b3026
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-12 20:36:32 +0600

    Partytown

--- src/components/ui/typewriter-effect.tsx
+++ src/components/ui/typewriter-effect.tsx
@@ -126,7 +126,7 @@ export const TypewriterEffectSmooth = ({
               {word.text.map((char, index) => (
                 <span
                   key={`char-${index}`}
-                  className={cn(`dark:text-white text-black `, word.className)}
+                  className={cn(`dark:text-white text-black content-center text-center justify-center`, word.className)}
                 >
                   {char}
                 </span>
