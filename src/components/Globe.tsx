c1995ad321d2dcbc1547ff5b313e70c78c79f564
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-14 22:23:21 +0600

    A bit of space

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -403,5 +403,5 @@ export function Globe() {
     },
   ];
 
-  return <World data={sampleArcs} globeConfig={globeConfig} className="mb-2"/>;
+  return <World data={sampleArcs} globeConfig={globeConfig} className="mb-8"/>;
 }
