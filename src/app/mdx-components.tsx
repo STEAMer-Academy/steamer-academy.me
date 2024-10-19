41e07cbdee928e513570fa1a7c8121decdc0d349
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-19 20:35:09 +0600

    MDX

--- src/app/mdx-components.tsx
+++ src/app/mdx-components.tsx
@@ -8,7 +8,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h3: ({ children }) => <h3 className="mb-2 text-2xl font-semibold leading-relaxed">{children}</h3>,
     p: ({ children }) => <p className="mb-4 align-left leading-loose">{children}</p>,
     a: ({ children }) => <a className="text-blue-600 hover:underline">{children}</a>,
-    img: (props: ImageProps) => <img className="rounded-lg shadow-md align-center container hover:shadow-md" {...props} />,
+    img: ({ alt, src, width, height }) => <img className="rounded-lg shadow-md align-center container hover:shadow-md" alt={alt} src={src} width={width} height={height} />,
     Image: (props: ImageProps) => <Image className="rounded-lg shadow-md align-center container hover:shadow-md" {...props} />,
     ...components,
   }
