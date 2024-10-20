a3ed979a59ea5730189f405ae33e87b75e36d28d
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-20 14:51:03 +0600

    Ratelimiting

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,7 +1,22 @@
 import { NextResponse } from 'next/server'
 import { redis, cache, BlogData, BlogCategory } from '@/lib/redis'
+import { Ratelimit } from '@upstash/ratelimit'
+import { headers } from 'next/headers'
 
-export async function GET(): Promise<NextResponse<BlogData>> {
+// Create a new ratelimiter, that allows 5 requests per day
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(5, '1 d'),
+})
+
+export async function GET(): Promise<NextResponse<BlogData | { error: string }>> {
+  const ip = headers().get('x-forwarded-for') ?? '127.0.0.1'
+  const { success } = await ratelimit.limit(ip)
+
+  if (!success) {
+    return NextResponse.json({ error: 'Rate limit exceeded' }, { status: 429 })
+  }
+
   const categories: BlogCategory[] = ['engineeringMds', 'englishMds', 'mathMds', 'scienceMds', 'technologyMds']
   const blogs: Partial<BlogData> = {}
 
@@ -25,4 +40,4 @@ export async function GET(): Promise<NextResponse<BlogData>> {
   return NextResponse.json(typedBlogs)
 }
 
-export const revalidate = 7200 
+export const revalidate = 7200
