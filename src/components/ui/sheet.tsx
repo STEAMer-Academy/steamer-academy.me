e6ff132b573638e7d3a68bbb992c827699594105
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-20 21:10:50 +0600

    Fix

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -50,7 +50,7 @@ const sheetVariants = cva(
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+  VariantProps<typeof sheetVariants> { }
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
@@ -63,6 +63,7 @@ const SheetContent = React.forwardRef<
       className={cn(sheetVariants({ side }), className)}
       {...props}
     >
+      <VisuallyHidden><SheetPrimitive.Title>Something</SheetPrimitive.Title></VisuallyHidden>
       {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
@@ -121,9 +122,6 @@ const SheetDescription = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
 >(({ className, ...props }, ref) => (
   <>
-    <VisuallyHidden asChild>
-      <SheetPrimitive.Title>Something</SheetPrimitive.Title>
-    </VisuallyHidden>
     <SheetPrimitive.Description
       ref={ref}
       className={cn("text-sm text-muted-foreground", className)}
