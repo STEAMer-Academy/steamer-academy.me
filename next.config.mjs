97c04c3b961a1cc494210c6cb3cf3244aa7eec5a
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-10-23 17:23:12 +0600

    Cloudflare YAY

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
+||||||| parent of 9ff250b (Cloudflare YAY)
+/** @type {import('next').NextConfig} */
+import withMDX from "@next/mdx"
+
+const mdxConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
+
+=======
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+// (when running the application with `next dev`), for more information see:
+// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
+
+/** @type {import('next').NextConfig} */
+>>>>>>> 9ff250b (Cloudflare YAY)
 const nextConfig = {
   reactStrictMode: true,
 
