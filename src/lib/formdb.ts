54f03d4a6692a9eb289dc9b57ff0d086db489a23
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-11-18 20:10:00 +0600

    Final Header Fix

--- src/lib/formdb.ts
+++ src/lib/formdb.ts
@@ -1,8 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
 
-const client = createClient({
-  url: process.env.TURSO_DATABASE_URL as string,
-  authToken: process.env.TURSO_AUTH_TOKEN as string,
+const libsql = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
 
-export default client;
+const adapter = new PrismaLibSQL(libsql);
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
