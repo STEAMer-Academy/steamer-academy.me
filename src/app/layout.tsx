dedab036091ed8b16a24b84595f3d2ab8f40e8e6
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-11-20 18:48:33 +0600

    Feat : posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import Script from "next/script";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark, shadesOfPurple } from "@clerk/themes";
 import { Client } from "appwrite";
+import { CSPostHogProvider } from "@/components/providers";
 
 export const metadata: Metadata = {
   title: "STEAMer Academy",
@@ -100,19 +101,21 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider appearance={{ baseTheme: [dark, shadesOfPurple] }}>
-          <CookieConsent />
-          <WebVitals />
-          <Toaster />
-          <Script id="clarity-script" strategy="afterInteractive">
-            {`
+          <CSPostHogProvider>
+            <CookieConsent />
+            <WebVitals />
+            <Toaster />
+            <Script id="clarity-script" strategy="afterInteractive">
+              {`
               (function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
             `}
-          </Script>
-          {children}
+            </Script>
+            {children}
+          </CSPostHogProvider>
         </ClerkProvider>
       </body>
     </html>
