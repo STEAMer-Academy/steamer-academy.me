a04fda7e60cf6dde8b6884d88713816788cb8ef4
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-12-12 14:20:53 +0600

    breaking: Remove Sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import { withSentryConfig } from "@sentry/nextjs";
-// import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 import bundleAnalyzer from "@next/bundle-analyzer";
 import withPWAInit from "@ducanh2912/next-pwa";
 import { withLogtail } from "@logtail/next";
@@ -12,8 +11,8 @@ const withPWA = withPWAInit({
   register: true,
 });
 
-const nextConfigFunction = () => {
-  // const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+const nextConfigFunction = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 
   const withBundleAnalyzer = bundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
@@ -29,8 +28,7 @@ const nextConfigFunction = () => {
       NEXT_PUBLIC_APP_VERSION: version,
     },
     reactStrictMode: true,
-    // assetPrefix: isDev ? undefined : "https://cdn.steameracademy.me",
-    productionBrowserSourceMaps: true,
+    assetPrefix: isDev ? undefined : "https://cdn.steameracademy.me",
 
     images: {
       remotePatterns: [
@@ -92,37 +90,4 @@ const nextConfigFunction = () => {
   return withBundleAnalyzer(withLogtail(withPWAInit(withPWA)(nextConfig)));
 };
 
-export default withSentryConfig(nextConfigFunction, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: "steamer-academy",
-  project: "javascript-nextjs-steamer-tu",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-});
+export default nextConfigFunction;
