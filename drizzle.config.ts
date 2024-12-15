af23cdae230ed714cb1ce58b808e4bc0dc079e3d
MuntasirSZN <muntasir.joypurhat@gmail.com>
2024-12-15 18:20:10 +0600

    fix: Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
-import "dotenv/config";
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+config({ path: '.dev.vars' });
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/lib/db/schema.ts",
+  schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
